6cb830fc723ac8b4cf1c2ad8f6b3145aafdd24ce
ramyadav10 <ramyadav.now@gmail.com>
2021-07-21 22:48:59 +0530

    Checking Attendence of the Employee

---
+++ EmpAttendence.java
@@ -0,0 +1,14 @@
+package com.practice;
+
+public class EmpAttendence {
+	public static void main(String[] args) {
+		int Is_FullTime = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == Is_FullTime)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is Absent");
+
+	}
+
+}
