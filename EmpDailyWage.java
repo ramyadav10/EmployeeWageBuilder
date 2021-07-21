b721537eb70959e171e6b9b8dad463e7658b5b29
ramyadav10 <ramyadav.now@gmail.com>
2021-07-21 22:52:16 +0530

    Checking Employee daily Wage

---
+++ EmpDailyWage.java
@@ -0,0 +1,22 @@
+package com.practice;
+
+public class EmpDailyWage {
+	public static void main(String[] args) {
+		int Is_FullTime = 1;
+		int Wage_per_hour = 20;
+		
+		int emphrs = 0;
+		int EmpWage = 0;
+		
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == Is_FullTime)
+			emphrs = 8;
+		else
+			emphrs = 0;
+		
+		EmpWage = Wage_per_hour * emphrs;
+		System.out.println("Employee wage is: " + EmpWage);
+	}
+
+}
+		
